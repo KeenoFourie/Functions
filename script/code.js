a9b6cce1b532d2165f39fb95691e61967418f624
KeenoFourie <keenofourie614@gmail.com>
2023-05-24 14:35:15 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,20 @@
+// // Question1
+// function greet() {
+//     console.log('Hello');
+// }
+
+// // Question2
+// function multiply(num1, num2) {
+//     console.log(num1 * num2);
+// }
+// multiply('5', '6')
+
+// // Question3
+// function tableOfFive(number) {
+//     return number * 5
+// }
+// console.log(tableOfFive(3));
+
+let x == 10
+let y = 3
+console.log(x+y);
